a0245cfc395357afb8a4dd56b638a66dab49c5ab
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-21 10:05:31 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,3 +1,14 @@
+
+function solution (angle){
+    return 0 < angle && angle < 90 ? 1 : angle === 90 ? 2 : 90 < angle && angle < 180 ? 3 : 4
+}
+
+/*
+switch문에서는 각 case에 범위를 직접 지정할 수 없음! 비교식이 오는 것도 옳지 않다.
+가독성을 고려해서 if-else문으로 하는게 좀 더 낫다고 생각했으나
+대부분의 경우에서는 삼항연산자가 가독성이 더 좋음.
+따라서 범위 지정 없이 삼항연산자로 풀이
+
 function solution(angle) {
     if(0 < angle && angle < 90){
         return 1;
@@ -9,7 +20,21 @@ function solution(angle) {
         return 4;
     }
 }
-/*
-switch문에서는 각 case에 범위를 직접 지정할 수 없음! (삼항연산자도 포함) 
-가독성을 고려해서 if-else문으로 하는게 낫다.
+
+switch문도 풀어봤으나 비교식이 오는 것이 옳지 않으므로 패스
+switch문은 단일 값과 각 case값을 비교하는데 사용된다.
+그러므로 비교할 값만 올 수 있음!
+
+function solution(angle) {
+    switch(true){
+        case 0 < angle && angle < 90 :
+            return 1;
+        case angle === 90 : 
+            return 2;
+        case 90 < angle && angle < 180 : 
+            return 3;
+        case angle === 180 : 
+            return 4;
+    }
+}
 */
