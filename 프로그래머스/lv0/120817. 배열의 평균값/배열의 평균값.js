035a843a24f7d20bd9adbf46c90be2393cf469a9
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-21 12:25:32 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,4 +1,20 @@
-function solution(numbers) {
-    const sum = numbers.reduce((acc, cur) => acc + cur);
-    return numbers.length === 0 ? 0 : sum / numbers.length;
+function solution(numbers){
+    const average = numbers.reduce((acc, cur) => acc + cur, 0) / numbers.length;
+    return average;
 }
+
+/*
+Array.prototype.reduce()
+배열의 각 요소에 대해 주어진 콜백 함수를 실행하면서 하나의 결과 값을 반환한다.
+배열 요소들을 순차적으로 처리하면서 누적된 값을 계산하는데 사용됨
+
+arr.reduce(callback[, initialValue])
+
+callback : 누적 계산을 수행하는 함수 4개의 인수를 받는다.
+    * accumulator : 누적 값을 저장하는 변수, 콜백 함수의 반환 값이 누적됨 
+                  (초기값을 설정하면 설정한 초기값을 사용하고 없는 경우 배열의 첫번째 요소가 초기값이 됨)
+    * currentValue : 현재 처리 중인 배열 요소 값.
+      currentIndex : 현재 처리 중인 배열 요소의 인덱스.
+      array : reduce()를 호출한 배열 자체.
+initialValue : 초기 누적 값. 지정하지 않으면 배열의 첫 번째 요소가 초기값으로 사용됨. 빈 배열에 reduce()를 호출하면 에러가 발생
+*/
