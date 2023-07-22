d781765d47c274b9ec2c908641172763ac3ef5ab
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-23 00:49:59 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,11 +1,22 @@
 function solution(strlist) {
+    return strlist.map(el => el.length);
+}
+/*
+원소의 길이를 담은 새로운 배열 생성
+배열 원소별로 length 값을 리턴해서 새 배열에 push
+
+function solution(strlist) {
     const strLength = [];
     for (const el of strlist){
         strLength.push(el.length);
     }
     return strLength;
 }
-/*
-원소의 길이를 담은 새로운 배열 생성
-배열 원소별로 length 값을 리턴해서 새 배열에 push
+
+------> map()메서드 사용하기 : 호출 배열의 모든 요소에서 제공된 함수를 호출한 결과로 채워진 새 배열을 만듦
+
+function solution(strlist) {
+    return strlist.map(el => el.length);
+}
+
 */
