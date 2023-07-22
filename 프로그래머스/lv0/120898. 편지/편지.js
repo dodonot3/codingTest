903f0ba76cba773810016094dfc95773b42cd7fa
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-23 05:04:12 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.js
@@ -0,0 +1,10 @@
+function solution(message) {
+    let strArr = message.split("");
+    return strArr.length * 2
+}
+
+/*
+strArr배열을 만듦
+message를 글자를 각각 나눠 strArr에 할당
+strArr의 length 값에 2를 곱하여, 가로 길이로 반환
+*/
