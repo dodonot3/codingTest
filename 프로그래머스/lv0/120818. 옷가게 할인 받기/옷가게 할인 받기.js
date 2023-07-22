579d8bdfa99c0e0021fdb349b6997abb36b40c75
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-22 18:50:42 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,15 @@
+function solution(price) {
+    return Math.floor(price < 100000 ? price 
+        : price >= 100000 && price < 300000 
+            ? price - price * 0.05 
+            : price >= 300000 && price < 500000
+                ? price - price * 0.1 
+                : price - price * 0.2);
+}
+/*
+가독성이 있는 삼항연산자로 풀이
+1. 누계가 10만 미만이면 price 그대로 반환
+2. 누계가 10만 이상이고 30만 미만이면 - 5% 할인
+3. 누계가 30만 이상이고 50만 미만이면 - 10% 할인
+4. 누계가 50만 이상이면 - 20% 할인
+*/
