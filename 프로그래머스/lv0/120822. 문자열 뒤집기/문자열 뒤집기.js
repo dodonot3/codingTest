7f18446e8c1c498b223be167ddfac5db649d3a4d
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-27 00:09:45 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,3 +1,11 @@
 function solution(my_string) {
     return [...my_string].reverse().join('');
 }
+/*
+------------------------->
+Array.prototype.reverse() 호출한 배열을 반전하고 원본 배열을 변형
+    array.reverse()
+
+Array.prototype.toReversed() 역순으로 정렬된 새로운 배열을 반환(ES2023 도입된 메서드)
+원본 배열을 해치는 reverse() 메서드를 보완
+*/
