ad4da8fd1d11a69e5039ef169f13cfdf4d90222c
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-26 23:52:04 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -8,3 +8,49 @@ function solution(my_string, n) {
     }
     return result;
 }
+
+/*
+문자열을 배열로 바꾸고
+결과를 변수에 담기
+반복문으로 배열 길이 만큼 반복하면서 n번만큼 같은 문자열 출력
+스트링 타입이므로 연산자를 이용해서 합치고 반환
+
+function solution(my_string, n) {
+    const strArr = [...my_string];
+    let result = '';
+    for (i=0; i< strArr.length; i++){
+        for (j=0; j<n; j++){
+            result += strArr[i];
+        }
+    }
+    return result;
+}
+
+---------------------------------> 스프레드 문법, map, repeat() 이용
+
+function solution(my_string, n) {
+    return [...my_string].map(text => text.repeat(n)).join('');
+}
+
+Array.prototype.map()
+배열의 각 요소에 대해 주어진 함수를 호출하고, 그 결과로 새로운 배열을 생성한다, 새로운 배열 반환(원본 배열 변경없이)
+
+map(callback(currentValue, index, array));
+    callback: 각 요소에 대해 호출될 함수로서, 새로운 값으로 매핑하는
+    currentValue: 배열에서 현재 처리 중인 요소의 값
+    index: 배열에서 현재 처리 중인 요소의 인덱스
+    
+-------------
+
+String.prototype.repeat() 문자열을 주어진 횟수만큼 반복해 붙인 새로운 문자열을 반환
+
+str.repeat(count);
+    count 문자열을 반복할 횟수.
+    
+---------------------------------> split(''), reduce() 이용
+
+function solution(my_string, n) {
+    return my_string.split('').reduce((acc,cur) => acc + cur.repeat(n),'');
+}
+
+*/
