d2ab573a1e0efafade475e5a19e1928b4f920c15
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-30 04:04:41 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -13,7 +13,15 @@ function solution(my_string) {
 
 **replace랑 replaceAll 차이를 몰라서 시간을 잡아먹음
 
----------------------replace vs replaceAll
+function solution(my_string) {
+    const vowel = ["a", "e", "i", "o", "u"];
+    for(const str of vowel){
+        my_string = my_string.replaceAll(str, "");
+    }
+    return my_string;
+}
+
+---------------------> replace vs replaceAll
 * replace() 첫 번째 일치하는 부분만 치환
 * replaceAll() 모든 일치하는 부분을 한꺼번에 치환
 
@@ -23,4 +31,20 @@ replace() 메서드:
 
 replaceAll() 메서드:
 문자열 전체에서 모든 일치하는 부분을 치환
+
+---------------------> 정규표현식
+처음 알게됨.. 얼마나 많이 쓰는지는 잘 모르겠다. (replaceAll 같은 함수가 나오는 거 보니..)
+
+function solution(my_string) {
+    return my_string.replace(/[aeiou]/g,'');
+}
+
+---------------------> filter 사용방법
+해보려 했는데 아이디어가 떠오르지 않았다. 해당 방법으로 가능하다.
+모음을 배열로 걸러내도 빈배열만 반환됨 includes를 꼭 해서 모음을 제외한 나머지 문자를 포함시켜줘야 한다. 
+
+function solution(my_string) {
+    return Array.from(my_string).filter(str => !['a','e','i','o','u'].includes(str)).join('');
+}
+
 */
