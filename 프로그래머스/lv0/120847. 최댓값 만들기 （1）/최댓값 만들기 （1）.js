c3da6b0f7ab79f1859a6a5f690d038310faedbd0
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-29 23:58:08 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,4 +1,12 @@
 function solution(numbers) {
-    let newArr = numbers.sort((a, b)=> b - a);
-    return newArr[0] * newArr[1];
+    numbers.sort((a, b)=> b - a);
+    return numbers[0] * numbers[1];
 }
+
+/*
+배열을 오름차순으로 정렬 바꾸기
+오름차순 0번째 인덱스와 1번째 인덱스 곱하기
+-> 최대값
+
+원본 배열을 바꾸지 않고 복사 배열로 정렬 하려면 ES2023 toSorted()사용
+*/
