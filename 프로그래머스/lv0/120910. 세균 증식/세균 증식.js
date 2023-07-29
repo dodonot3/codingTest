81c51c5b4d78d46b461c4bd3fe03bcf5e6762e3e
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-30 08:21:38 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,10 +1,23 @@
 function solution(n, t) {
+    return n * Math.pow(2, t);
+}
+/*
+n이 2배씩 t만큼 제곱됨
+
+function solution(n, t) {
     let germsNum = 0;
     for(let i=0; i<t; i++){
         germsNum = n*2**t;
     }
     return germsNum;
 }
-/*
-n이 2배씩 t만큼 제곱됨
+
+----------------> Math.pow() 사용
+Math.pow(base, exponent)
+    base 에 exponent를 제곱한 값을 반환합니다.
+    
+function solution(n, t) {
+    return n * Math.pow(2, t);
+}
+
 */
