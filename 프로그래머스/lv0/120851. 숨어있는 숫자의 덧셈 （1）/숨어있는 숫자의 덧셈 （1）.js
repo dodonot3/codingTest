b3a07b07637a0c008cf919431fd3ce44930b7512
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-30 07:51:41 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -7,10 +7,27 @@ function solution(my_string) {
     }
     return sum;
 }
+
 /*
 my_string 에서 number type만 filter
 for of 자연수의 합 구하기
 
 * typeof 연산자는 변수의 데이터 타입을 확인하는 용도로 사용. 
 * 문자를 숫자와 비교할 때는, isNaN 사용. 숫자가 아니면 true 반환
+
+function solution(my_string) {
+    let numberStr = [...my_string].filter(el => !isNaN(el));
+    // isNaN 숫자가 아니면 true 반환
+    let sum = 0;
+    for (const el of numberStr){
+       sum += +el;
+    }
+    return sum;
+}
+
+---------------> 기본적인 Number() 함수를 잊고 있었어!!!
+function solution(my_string) {
+    return [...my_string].reduce((acc, cur) => Number(cur) ? acc + +cur : acc, 0);
+}
+
 */
