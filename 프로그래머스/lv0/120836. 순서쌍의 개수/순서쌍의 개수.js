f7e6fc5df4ac83ef9e3c7467223bd960e03a6105
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-07-30 00:08:54 +0900

    [level 0] Title: 순서쌍의 개수, Time: 6.07 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,25 +1,24 @@
 function solution(n) {
     let numArr = [];
-    for (let a = 0; a < n; a++){
+    for (let a = 1; a <= n; a++){
         if(n % a === 0) numArr.push(a);
     }
-   
-    return [...new Set(numArr)].length + 1
+    return [...new Set(numArr)].length;
 }
 
 /*
 a와 b는 n의 약수(나머지가 0)
 a와 b는 n보다 작다.
 
+for 반복문으로, n을 a로 1부터 n까지 나눠서 
+나머지가 0인 a를 새 배열에 push
+배열의 길이 값을 반환
+
 function solution(n) {
     let numArr = [];
-    for (let i = 0; i < n; i++){
-        n % a === 0 && n % b === 0
-        && a <= n
-        && b <= n
-        && a * b === n
-        numArr.push(a);
-    } 
-    return numArr.length;
+    for (let a = 1; a <= n; a++){
+        if(n % a === 0) numArr.push(a);
+    }
+    return [...new Set(numArr)].length;
 }
 */
