2ee2d02e4d3183805d36a2f11fb733934e3d728e
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-08-02 11:46:28 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,20 +1,4 @@
 function solution(cipher, code) {
-    let decoding = "";
-    for(let i = code - 1; i<cipher.length; i+=code){
-        decoding += cipher[i];
-    }
-    return decoding;
-}
-/*
-해독암호 새배열 만들기
-cipher 문자열을 배열로
-index code* i-1번째만 새배열에 push, 해독암호 배열 리턴
-
-** undefined 일때도 예외 처리 해줌 if(cipher[code*i-1] == undefined)
-** return으로 하니 최종적인 ''+undefined = undefined가 반환됨
-** break;로 예외처리
-
-function solution(cipher, code) {
     let decoding = [];
     cipher = [...cipher];
     for(let i=1; i<=cipher.length; i++){
@@ -23,21 +7,9 @@ function solution(cipher, code) {
     }
     return decoding;
 }
-------------------------------------> index 초기화 할때 매개변수 이용해서 점화식 없애기
-
-function solution(cipher, code) {
-  var answer = "";
-  for (let i = code - 1; i < cipher.length; i += code) {
-    answer += cipher[i];
-  }
-  return answer;
-}
-
-------------------------------------> 문자열 split로 나눠서 filter를 인덱스로 사용하는 법
-function solution(cipher, code) {
-    return cipher.split('').filter((_, index) => (index + 1) % code === 0).join('');
-}
-
-
+/*
+cipher 배열로
+index code* i-1번째만 새배열에 push
+새배열 join 리턴
 
 */
