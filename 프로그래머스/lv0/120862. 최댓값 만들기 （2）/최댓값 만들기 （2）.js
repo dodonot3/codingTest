eeda155cc54dac3e33630df2d4eb17cd2dc08f18
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-08-04 23:50:48 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    numbers.sort((a, b) => b-a);
+    let a = numbers[0] * numbers[1];
+    let b = numbers[numbers.length-1] * numbers[numbers.length-2];
+    return (Math.max(a, b));
+}
