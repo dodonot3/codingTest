d91d914196742fc4fd458cb7055ee5ec48824af2
dodonot3 <105469531+dodonot3@users.noreply.github.com>
2023-08-05 23:57:44 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,6 +1,15 @@
 function solution(box, n) {
+    
     let width = Math.floor(box[0]/n);
     let depth = Math.floor(box[1]/n);
     let height = Math.floor(box[2]/n);
     return width * depth * height;
 } 
+
+/*
+구조 분해 할당
+function solution(box, n) {
+    let [width, length, height] = box;
+    return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n);
+}
+*/
